8e4619f947a62c1fd6aa45db14ddaf8e32a9439a
Kirti Vasdev <kirtivasdev12@gmail.com>
2020-07-03 00:58:02 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Datagov_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
